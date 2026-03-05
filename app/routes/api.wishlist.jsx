2358cbb793056d0920fbb1b8f5fbf546e092aea7
sayantan Maity <sayantan.maity@fortmindz.in>
2026-03-05 17:48:11 +0530

    5march

--- app/routes/api.wishlist.jsx
+++ app/routes/api.wishlist.jsx
@@ -1,68 +1,58 @@
 import db from "../db.server"
 import { cors } from "remix-utils/cors"
-import { action } from "./webhooks.app.scopes_update";
 
-export async function loader({ request }) {
-  const url = new URL(request.url);
-  const customerId = url.searchParams.get("customerId");
-  const shopUrl = url.searchParams.get("shop");
-  const productId = url.searchParams.get("productId");
-
-  if (!customerId || !shopUrl || !productId) {
-    return Response.json({
-      message: "Missing required parameters",
-      method: "GET",
-    })
-  }
-
-  const wishlist = await db.wishlist.findMany({
-    where: {
-      customerId: customerId,
-      shop: shopUrl,
-      productId: productId,
-    },
-  });
-
-  const response = Response.json({
+export async function loader() {
+  const data = {
     ok: true,
-    message: "Success",
-    data: wishlist,
-  });
-
-  return cors(request, response);
-
+    message: "wish api"
+  };
+  return Response.json(data);
 }
 
-export async function action_1({ request }) {
-  const formData = Object.fromEntries(await request.formData());
+export async function action({ request }) {
+  const method = request.method;
+  let data = await request.formData();
+  data = Object.fromEntries(data);
+  const productId = data.productId;
+  const customerId = data.customerId;
+  const shop = data.shop;
 
-  const customerId = formData.customerId;
-  const shopUrl = formData.shop;
-  const productId = formData.productId;
 
-  const _action = formData._action;
-
-  if (_action === "CREATE") {
-    await db.wishlist.create({
-      data: {
-        customerId: customerId,
-        shop: shopUrl,
-        productId: productId,
-      },
-    });
-  } else if (_action === "DELETE") {
-    await db.wishlist.deleteMany({
-      where: {
-        customerId: customerId,
-        shop: shopUrl,
-        productId: productId,
-      },
+  if (!productId || !customerId || !shop) {
+    return Response.json({
+      ok: false,
+      message: "Missing required fields: shop, productId, customerId",
+      method: method
     });
   }
 
-  return Response.json({
-    ok: true,
-    message: "Success",
-  });
+  switch (method) {
+    case "POST":
+
+      const wishlist = await db.wishlist.create({
+        data: {
+          customerId,
+          productId,
+          shop
+        }
+      });
+
+      const response = Response.json({
+        message: "Product added to wishlist",
+        method: "POST",
+        wishlist: wishlist
+      });
+
+      return cors(request, response);
+
+    case "PATCH":
+      return Response.json({
+        message: "Success",
+        method: "patch"
+      });
+
+    // case "DELETE":
+
+  }
 
 }
